c3d0cefd9caa8928eb5896df0522d2da76be3195
yoon32-vitoria <vitoria.reis.neko@gmail.com>
2025-09-30 15:31:40 -0300

    Add files via upload

---
+++ JavaScript.JS/script.js
@@ -0,0 +1,14 @@
+//arrays - > vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maçã", "Melancia"];//array tamanho 3
+console.log(frutas);
+console.log(frutas[1])
+
+let numeros = new Array(10,2,6,7);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Vitória";
+console.log(arrayVazio);
